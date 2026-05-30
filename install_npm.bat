5875723d0ba04bac4827d1f362c0c0be0c407a22
denis22078533-coder <denis22078533@example.com>
2026-05-30 14:41:46 +0300

    9999

--- install_npm.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d C:\Users\ЗС\Documents\Downloads\bd-main
-set NODE=C:\Users\ЗС\Documents\Downloads\bd-main\nodejs\node-v20.11.0-win-x64\node.exe
-set NPM=C:\Users\ЗС\Documents\Downloads\bd-main\nodejs\node-v20.11.0-win-x64\node_modules\npm\bin\npm-cli.js
-"%NODE%" "%NPM%" config set prefix "C:\Users\ЗС\Documents\Downloads\bd-main" --location=project
-"%NODE%" "%NPM%" install
